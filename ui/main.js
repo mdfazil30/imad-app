3deb3f6a86abd541f58d2134635f459844943f87
mdfazil30 <mdfazil30@gmail.com>
2017-08-30 12:41:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var btn = document.getElementById('counterBtnId');
-var counter;
+var counter = 0;
 btn.onclick = function() {
     counter = counter + 1;
     var span = document.getElementById('counterValue');
