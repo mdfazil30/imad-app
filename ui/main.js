faee4d74509663e456306bfe24a212c3ecaba164
mdfazil30 <mdfazil30@gmail.com>
2017-08-30 13:08:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,15 +3,10 @@ var btn = document.getElementById('counterBtnId');
 // var counter = 0;
 btn.onclick = function() {
     var request = new XMLHttpRequest();
-    console.log("check1");
     request.onreadystatechange = function() {
-        console.log("check2");
         if(request.readyState === XMLHttpRequest.DONE) {
-            console.log("check3");
             if(request.status === 200) {
-                console.log("check4");
                 var counter = request.responseText;
-                console.log(counter.toString());
                 var span = document.getElementById('counterValue');
                 span.innerHTML = counter.toString();
             }
