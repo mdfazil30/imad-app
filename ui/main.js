7717324d70592c49170ecc76817cb06bc4300b93
mdfazil30 <mdfazil30@gmail.com>
2017-08-30 12:56:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,18 @@
 console.log('Loaded!');
 var btn = document.getElementById('counterBtnId');
-var counter = 0;
+// var counter = 0;
 btn.onclick = function() {
-    counter = counter + 1;
-    var span = document.getElementById('counterValue');
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONe) {
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('counterValue');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    // counter = counter + 1;
+    request.open('GET', 'http://mdfazil30.imad.hasura-app.io/', true);
+    request.send(null);
 };
